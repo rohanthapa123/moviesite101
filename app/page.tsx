a5780f15036cff06c41beeaf8ec651e0ed7a493c
Rohan Thapa <thaparohan2019@gmail.com>
2024-02-29 20:54:36 +0545

    folder setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Navbar />
       <h1 className=" text-4xl text-green-600 font-sans">Its going to be a free and ads free movie watching site (^_^) </h1>
 
 
