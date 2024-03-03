f8763573e4e313ce57b31dcdc1aa946e262982a8
milap-magar <milapmagar12@gmail.com>
2024-03-03 07:23:23 +0545

    trying to add button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+// import Button from "./Button";
 
 const Navbar = () => {
   const [navState, setNavState] = useState(false);
@@ -20,27 +21,30 @@ const Navbar = () => {
   }, []);
 
   const headerClass = navState
-    ? "fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-50 blur-xl bg-slate-300 bg-slate-300 cursor-pointer"
+    ? "fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z`-`50 blur-xl bg-slate-300 bg-slate-300 cursor-pointer"
     : "w-full h-[60px] absolute top-0 left-0 right-0 opacity-100 z-[200] bg-slate-300 cursor-pointer";
 
   return (
     <>
       <header className={headerClass}>
-        <div className="absolute top-3">
-          <figure className="text-black">LOGO</figure>
-        </div>
-        <ul className="md:flex justify-end items-center gap-10 text-black absolute top-4 right-20 hidden">
-          <li>Movies</li>
-          <li>Anime</li>
-          <li>Drama</li>
-        </ul>
-        <div className="input-section">
-          <input
-            type="text"
-            name="search_bar"
-            className={`lg:w-96 w-48 h-8 rounded-md focus:outline-none text-slate-800 text-center focus:text-start absolute top-4 lg:right-[450px] right-0`}
-            placeholder="Search movies.. 🔎 "
-          />
+        <div className="relative">
+          <div className="absolute top-3">
+            <figure className="text-black">LOGO</figure>
+          </div>
+          <ul className="md:flex justify-end items-center gap-10 text-black absolute top-4 right-20 hidden">
+            <li>Movies</li>
+            <li>Anime</li>
+            <li>Drama</li>
+          </ul>
+          <div className="relative">
+            <input
+              type="text"
+              name="search_bar"
+              className={`lg:w-96 w-48 h-8 rounded-md focus:outline-none text-slate-800 text-center focus:text-start absolute top-4 lg:right-[450px] right-0`}
+              placeholder={`Search movies.. 🔎`}
+            />
+            {/* <Button value="search" /> */}
+          </div>
         </div>
       </header>
     </>
