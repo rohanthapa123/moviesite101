f63f2359da4055b96d449452a8bbd5a0c628ddbf
milap-magar <milapmagar12@gmail.com>
2024-03-03 15:18:16 +0545

    logo added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
 // import Button from "./Button";
 
 const Navbar = () => {
@@ -22,16 +23,23 @@ const Navbar = () => {
 
   const headerClass = navState
     ? "fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z`-`50 blur-xl bg-slate-300 bg-slate-300 cursor-pointer"
-    : "w-full h-[60px] absolute top-0 left-0 right-0 opacity-100 z-[200] bg-slate-300 cursor-pointer";
+    : "w-full h-[60px] absolute top-0 left-0 right-0 opacity-100 z-[200] bg-slate-700 cursor-pointer";
 
   return (
     <>
       <header className={headerClass}>
         <div className="relative">
-          <div className="absolute top-3">
-            <figure className="text-black">LOGO</figure>
+          <div className="absolute top-2 left-5">
+            <figure className="">
+              <Image
+                src="/images/logo2.png"
+                alt="Logo Image"
+                height={40}
+                width={50}
+              />
+            </figure>
           </div>
-          <ul className="md:flex justify-end items-center gap-10 text-black absolute top-4 right-20 hidden">
+          <ul className="md:flex justify-end items-center gap-10 text-white absolute top-4 right-20 hidden">
             <li>Movies</li>
             <li>Anime</li>
             <li>Drama</li>
@@ -40,7 +48,7 @@ const Navbar = () => {
             <input
               type="text"
               name="search_bar"
-              className={`lg:w-96 w-48 h-8 rounded-md focus:outline-none text-slate-800 text-center focus:text-start absolute top-4 lg:right-[450px] right-0`}
+              className={`lg:w-96 w-48 h-8 p-4 rounded-md focus:outline-none text-slate-800 text-center focus:text-start absolute top-4 lg:right-[450px] right-0`}
               placeholder={`Search movies.. 🔎`}
             />
             {/* <Button value="search" /> */}
